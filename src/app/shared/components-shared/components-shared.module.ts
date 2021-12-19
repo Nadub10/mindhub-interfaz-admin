ea54905733631b5b916aeb3c65d7db282e4201fb
nadub10 <nadia.batyk@gmail.com>
2021-12-18 22:07:03 -0300

    feat:estilos en registro wii

--- src/app/shared/components-shared/components-shared.module.ts
+++ src/app/shared/components-shared/components-shared.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { MaterialModule } from '../material/material.module';
+import { AuthRoutingModule } from 'src/app/auth/auth-routing.module';
+import { DashboardRoutingModule } from 'src/app/dashboard/dashboard-routing.module';
 
 
 
@@ -11,7 +13,9 @@ import { MaterialModule } from '../material/material.module';
   ],
   imports: [
     CommonModule,
-    MaterialModule
+    MaterialModule,
+    AuthRoutingModule,
+    DashboardRoutingModule
   ],
   exports:[
     HeaderComponent
