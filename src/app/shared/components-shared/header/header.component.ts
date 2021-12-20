4a2552926630fd6d216f41b1f8fed69bf177698d
nadub10 <nadia.batyk@gmail.com>
2021-12-20 12:06:23 -0300

    feat:alto arreglado finally, ya anda el header y cerrar sesion wii

--- src/app/shared/components-shared/header/header.component.ts
+++ src/app/shared/components-shared/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { AccederLocalStorageService } from 'src/app/dashboard/services/acceder-local-storage.service';
-
+import { Router } from '@angular/router';
+import Swal from'sweetalert2';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -9,12 +10,31 @@ import { AccederLocalStorageService } from 'src/app/dashboard/services/acceder-l
 export class HeaderComponent implements OnInit {
 
   @Output() sidenavToggle = new EventEmitter();
-  constructor() { }
-  
+  constructor(private route: Router) { }
+  adminLogeado =JSON.parse(localStorage.getItem('adminLogged')!)
+  fullNameAdmin=this.adminLogeado?.fullName || ''
   ngOnInit(): void {
   }
   onToggleSidenav(){
     this.sidenavToggle.emit();
+  }
+  cerrarSesion(){
+    Swal.fire({
+      icon:'warning',
+      title: '¿Estás seguro de cerrar sesión?',
+      
+      showCancelButton: true,
+      confirmButtonText: 'Si',
+      
+    }).then((result) => {
+      
+      if (result.isConfirmed) {
+        this.route.navigate(['/auth']);
+   
+        localStorage.clear();
+      }
+    })
+    
   }
 
 }
